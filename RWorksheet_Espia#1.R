2e1f65791b1f9e7a449b3218452840854a148e69
Neo Michael Kenneth Espia <kanekiken0728@gmail.com>
2023-09-20 08:42:34 +0000

    Hello

---
+++ RWorksheet_Espia#1.R
@@ -0,0 +1,55 @@
+age <- c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29,35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41,51, 35, 24, 33, 41.)
+age
+
+#1.How many data points?
+length(age)
+
+#2.Find the reciprocal of the values for age.
+reciprocal_age <- 1/age
+reciprocal_age
+
+#3.What happen to the new_age?
+new_age <- c(age, 0, age)
+new_age
+
+#4.Sort the values for age.
+sort(age)
+
+
+#5.Find the minimum and maximum value for age.
+min_age <- min(age)
+min_age
+
+max_age <- max(age)
+max_age
+
+#6.Set up a vector named data, consisting of 2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3,
+#2.5, 2.3, 2.4, and 2.7
+data <- c(2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3, 2.5,2.3, 2.4, 2.7)
+data
+
+#7.Generates a new vector for data where you double every value of the data.
+doubled_data <- data * 2
+doubled_data
+
+#8.Generate a sequence for the following scenario:
+#8.1
+seq_1_to_100 <- 1:100
+seq_1_to_100
+
+#8.2
+seq_20_to_60 <- 20:60
+seq_20_to_60
+
+#8.3
+mean_20_to_60 <- mean(20:60)
+mean_20_to_60
+
+#8.4
+sum_51_to_91 <- sum(51:91)
+sum_51_to_91
+
+#8.5
+seq_1_to_1000 <- 1:1000
+seq_1_to_1000
+
